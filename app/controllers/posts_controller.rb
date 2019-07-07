00991bdf11aa11503fc45801149524cd5d9da595
kia kamangar <kia.kamangar@gmail.com>
2019-07-07 19:49:16 +0430

    Added all file and end of tumblr project

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,25 +1,57 @@
 class PostsController < ApplicationController
+	before_action :authenticate_user!, 	except: [:index, :show]
+
+
 	def index
 		@posts = Post.all.order('created_at DESC')
 	end
 
 	def new
-		
+		@post = Post.new
 	end
 
 	def create
 		@post = Post.new(post_params)
-		@post.save
-
-		redirect_to @post
+		
+		if @post.save
+			redirect_to @post
+		else
+			render 'new'
+		end
 	end
 
 	def show
 		@post = Post.find(params[:id])
 	end
 
-	private
-	def post_params
-		params.require(:post).permit(:title, :body)
+	def edit
+		@post = Post.find(params[:id])
 	end
-end
+
+	def update
+		@post = Post.find(params[:id])
+		
+		if @post.update(params.require(:post).permit(:title, :body))
+			redirect_to @post
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@post.destroy
+
+		redirect_to root_path
+
+	end
+
+	private
+	
+		def post_params
+			params.require(:post).permit(:title, :body)
+		end
+
+		def find_post
+			@post = Post.find(params[:id])
+		end
+	end
